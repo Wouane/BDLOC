6990564d8e4d40af6ead6b198c051127a020b7bd
Florian Wouane <pro.florian.michel@gmail.com>
2015-08-24 15:47:42 +0200

    edit routes for bdloc website

--- app/routes.php
+++ app/routes.php
@@ -1,11 +1,57 @@
 <?php
 	// merci de ne pas toucher au fichier routes sans accord du PATRON :D :p 
 	$w_routes = array(
-	// --------------------------PUBLIC / USER ------------------------------------
+
+		// ================================================================================================
+		// --------------------------PUBLIC / USER --------------------------------------------------------
+		// ================================================================================================
+
+		//accueil de l'admin --------------------------------------------------------------------------
 		['GET', '/', 'Default#home', 'home'],
-		['GET|POST', '/catalogue/', 'Default#catalogue', 'catalogue'],
+
+		//Inscription de l'utilisateur ----------------------------------------------------------------
 		['GET|POST', '/inscription/', 'User#register', 'inscription'],
+
+		//Connexion de l'utilisateur ------------------------------------------------------------------	
 		['GET|POST', '/connexion/', 'User#login', 'connexion'],
-	// --------------------------PRIVATE / ADMIN ------------------------------------
+
+		//Page de Mot de passe oublié -----------------------------------------------------------------
+		['GET|POST', '/connexion/mot-de-passe-oublie/', 'User#forgotPassword', 'oublieMotdepasse'],
+
+		//Paramètres du profil de l'utilisateur -------------------------------------------------------
+		['GET|POST', '/catalogue/profil/parametres', 'User#parameters', 'parametres'],
+
+		//Page catalogue-------------------------------------------------------------------------------
+		['GET|POST', '/catalogue/', 'Default#catalogue', 'catalogue'],
+
+		//Page panier de l'utilisateur ----------------------------------------------------------------
+		['GET|POST', '/catalogue/panier/', 'User#cart', 'panier'],
+
+		//Page contact de l'utilisateur-----------------------------------------------------------------
+		['GET|POST', '/catalogue/contact/', 'User#contact', 'contact'],
+
+		//Page profil de l'utilisateur-----------------------------------------------------------------
+		['GET|POST', '/catalogue/profil/', 'User#profil', 'profile'],
+
+		//Page déconnexion de l'utilisateur -----------------------------------------------------------	
+		['GET', '/deconnexion/', 'User#logout', 'deconnexion'],
+
+	// ================================================================================================
+	// --------------------------PRIVATE / ADMIN ------------------------------------------------------
+	// ================================================================================================
+
+		// Creer un nouvel administrateur -------------------------------------------------------------
+		['GET|POST', '/admin/admin-control/inscription/', 'Admin#register', 'register_admin'],
+
+		// Panneau de controle ------------------------------------------------------------------------
+		['GET|POST', '/admin/control-pannel/', 'Admin#control', 'control_admin'],
+
+		// connexion login ----------------------------------------------------------------------------
+		['GET|POST', '/admin/connexion/', 'Admin#login', 'connexion_admin'],
+
+		//Déconnexion----------------------------------------------------------------------------------
+		['GET', '/admin/deconnexion/', 'Admin#logout', 'deconnexion_admin'],
+
+		//---------------------------------------------------------------------------------------------	
 
 	);
