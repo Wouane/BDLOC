62b6f80b69c348a50c11000d00041f3ec1d550ab
Florian Wouane <pro.florian.michel@gmail.com>
2015-08-24 15:08:00 +0200

    UserManager.php verification Mail / pseudo

---
+++ app/Manager/UserManager.php
@@ -0,0 +1,25 @@
+<?php 
+	namespace Manager;
+
+	class UserManager extends \W\Manager\UserManager
+	{
+		public function checkMail($email)
+		
+		{
+			$sql = "SELECT email FROM users WHERE email = :email";
+	    	$sth = $dbh->prepare($sql);
+	    	$sth->execute(array(":email" => $email));
+	    	$foundEmail = $sth->fetchColumn();
+	    	return $foundEmail;
+		}
+
+		public function checkUsername($username)		
+		{
+			$sql = "SELECT username FROM users WHERE username = :username";
+	    	$sth = $dbh->prepare($sql);
+	    	$sth->execute(array(":username" => $username));
+	    	$foundUsername = $sth->fetchColumn();
+	    	return $foundUsername;
+		}
+
+	}
