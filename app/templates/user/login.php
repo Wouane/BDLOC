612bef3897652d323e4809c613b06e5f052aaee9
Florian Wouane <pro.florian.michel@gmail.com>
2015-08-24 15:07:31 +0200

    edit login.php

---
+++ app/templates/user/login.php
@@ -0,0 +1,18 @@
+<?php $this->layout('layout', ['title' => 'Login !']) ?>
+
+<?php $this->start('main_content') ?>
+
+	<h2>Connectez-vous</h2>
+		<form method="POST" name="register" id="register" novalidate>
+			<div class="form-group">
+			    <label for="username">Votre Pseudo</label>
+			    <input name='username' type="text" class="form-control" id="username" value="<?= $username ?>">
+			</div>
+			<div class="form-group">
+			    <label for="password">Votre mot de passe</label>
+			    <input name='password' type="password" class="form-control" id="password" value="">
+			</div>			
+		  <button type="submit" class="btn btn-default">Valider</button>
+		  <div class="warning"><?php echo $error; ?></div>
+		</form>	
+<?php $this->stop('main_content') ?>
