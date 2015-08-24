aa0e9c33ab2400ff61646362ccf09e318220a5dd
Florian Wouane <pro.florian.michel@gmail.com>
2015-08-24 14:59:52 +0200

    edit routes

--- app/routes.php
+++ app/routes.php
@@ -1,8 +1,13 @@
 <?php
 	
 	$w_routes = array(
+		// --------------------------PUBLIC / USER ------------------------------------
 		['GET', '/', 'Default#home', 'home'],
 		['GET|POST', '/catalogue/', 'Default#catalogue', 'catalogue'],
 		['GET|POST', '/user/', 'User#register', 'inscription'],
 		['GET|POST', '/user/', 'User#login', 'login'],
+		['GET|POST', '/user/', 'User#login', 'login'],
+
+
+		// --------------------------PRIVATE / ADMIN ------------------------------------
 	);
