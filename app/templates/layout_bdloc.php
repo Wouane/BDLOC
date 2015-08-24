41d7d863a6fdaa86a029fe1003e074ef02e7faed
Alexandre <duparc.alexandre94@gmail.com>
2015-08-24 15:21:01 +0200

    add layout_bdloc

---
+++ app/templates/layout_bdloc.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title><?= $this->e($title) ?></title>
+
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/reset.css') ?>">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<!-- <link rel="stylesheet" type="text/css" href="<?= $this->assetUrl('css/style.css') ?>"> -->
+</head>
+<body>
+	<div class="container">
+		<header>
+			<h1><?= $this->e($title) ?></h1>
+			<section>
+				<?= $this->section('header') ?>
+			</section>	
+		</header>
+
+		<section>
+			<?= $this->section('main_content') ?>
+		</section>
+
+		<footer>
+		</footer>
+	</div>
+</body>
+</html>
