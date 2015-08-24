58359d948d06cf2d113d3a19c98ab1e9439a7604
Alexandre <duparc.alexandre94@gmail.com>
2015-08-24 16:27:01 +0200

    Created contact

---
+++ app/templates/default/Contact.php
@@ -0,0 +1,25 @@
+<form action="contact_handler.php" method="POST">
+	<div>
+		<label for="nom">* Nom complet</label>
+		<input type="text" id="nom">
+	</div>
+
+	<div>
+		<label for="email">* Adresse email</label>
+		<input type="email" id="email">
+	</div>
+
+	<div>
+		<label for="Objet">* Objet</label>
+		<input type="Objet" id="Objet">
+	</div> 
+
+	<div>
+		<label for="Message">* Message :</label>
+		<textarea id="message"></textarea>
+	</div>
+
+	<div>
+		<p>* Champs obligatoire</p>
+	</div>
+</form>
