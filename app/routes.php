f6e72097d26e8449ca5dde706fe485bc574d13e7
Web-God <alvardsandz@gmail.com>
2015-08-24 14:41:10 +0200

    created routes.php for user

--- app/routes.php
+++ app/routes.php
@@ -2,6 +2,7 @@
 	
 	$w_routes = array(
 		['GET', '/', 'Default#home', 'home'],
-
 		['GET|POST', '/catalogue/', 'Default#catalogue', 'catalogue'],
+		['GET|POST', '/user/', 'User#register', 'inscription'],
+		['GET|POST', '/user/', 'User#login', 'login'],
 	);
