293d8b56c854d100115be3ed98e125a447e8e305
Alexandre <duparc.alexandre94@gmail.com>
2015-08-25 16:43:45 +0200

    Edit profil

--- app/templates/user/profile.php
+++ app/templates/user/profile.php
@@ -1,19 +1,52 @@
-<?php $this->layout('layout_bdloc', ['title' => 'Bdloc']) ?>
+<?php $this->layout('layout_bdloc', ['title' => 'Profil']) ?>
 
 <?php $this->start('main_content'); ?>
 
-<h1>Profil</h1>
+	<form method="POST" enctype="multipart/form-data">
+		
+	<div class="form-modify">
+		<label for="picture">Modifiez votre image :</label>
+		<input type="file" name="picture" id="picture" />
+	</div>
 
-<form method="POST" action="handle_profile.php" enctype="multipart/form-data">
+	<h1>Modifiez les informations générales</h1>
+	
+	<div class="form-modify">
+		<label for="email">Email</label>
+		<input name="email" type="email" class="form_modify">
+	</div>
+
+	<div class="form-modify">
+		<label for="username">Pseudo</label>
+		<input name="username" type="text" class="form_modify">
+	</div>
+
+	<div class="form-modify">
+		<label for ="name">Nom</label>
+		<input name="name" type="text" id="name" class="form_modify">
+	</div>
+
+	<div class="form-modify">
+		<label for="firstname">Prénom</label>
+		<input name="firstname"type="text" id="firstname" class="form_modify">
+	</div>
+
+	<div class="form-modify">
+		<label for="zipcode">Code postal</label>
+		<input name="zipcode" type="text" id="zipcode" class="form_modify">
+	</div>
 		
-		<label for="pic">Choisissez une image :</label>
-		<input type="file" name="pic" id="pic" />
-		
-		<input type="submit" value="Envoyer !" />
+	<div class="form-modify">
+		<label for="streetnumber">Numéro</label>
+		<input name="streetnumber" type="text" id="streetnumber" class="form_modify">
+
+	<div class="form-modify">
+		<label for="adress">Adresse</label>
+		<input name="adress" type="text" id="adress" class="form_modify">
+	</div>
+
+	<button type="submit" class="btn btn-modify">Valider</button>
 
 	</form>
-
-
-
 
 <?php $this->stop('main_content'); ?>
