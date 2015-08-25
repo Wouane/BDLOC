a6ed5e0925e6692a100f81ecaff5ffabe415a9fe
Florian Wouane <pro.florian.michel@gmail.com>
2015-08-25 16:25:48 +0200

    add header to layout_bdloc and decomment style.css

--- app/templates/layout_bdloc.php
+++ app/templates/layout_bdloc.php
@@ -6,21 +6,46 @@
 
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/reset.css') ?>">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-	<!-- <link rel="stylesheet" type="text/css" href="<?= $this->assetUrl('css/style.css') ?>"> -->
+	<link rel="stylesheet" type="text/css" href="<?= $this->assetUrl('css/style.css') ?>">
 </head>
 <body>
-	<div class="container">
-		<header>
-			<h1><?= $this->e($title) ?></h1>
-			<section>
-				<?= $this->section('header') ?>
-			</section>	
-		</header>
+	<header>
+		<nav class="navigation">
+			<div class="BD">
+				<a href="#"><i class="fa fa-book"></i> Les BDs !</a>
+			</div>
+			<!-- LIEN PANIER -->
+			<div class="panier"> 
+				<a href="<?= $this->url('panier') ?>" title="panier"><i class="fa fa-shopping-cart"></i> Mon panier</a>
+			</div>
+			<!-- LIEN MON COMPTE -->
+			<div class="compte">
+				<a href="<?= $this->url('profile') ?>" title="profil"><i class="fa fa-user"></i> Mon compte</a>
+			</div>
+		</nav>
+			<!-- ESPACE USER -->
+		<div class="logout">
+  			<h1>Bonjour <?= $w_user['username']; ?> !</h1>
+  			<div class="profil-picture">
+  				<p>ICI insertion petite image profil (avatar)</p>
+  			</div>
+  			<!-- DECONNEXION -->
+ 			<input type="submit" value="Déconnexion">
+ 		</div>
+	</header>
 
+<hr>
+<br/>
+
+	<div class="container">
 		<section>
 			<?= $this->section('main_content') ?>
 		</section>
+	</div>
 
+<br/>		
+<hr>
+<br/>
 		<footer class="footer-layout-bdloc-nomenu">
 			<p>Facebook | Twitter | Mentions Légales | Contactez-nous </p>
 		</footer>
