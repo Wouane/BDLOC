6ae55909a41c478647eb39af89ce3013af7c13c8
Florian Wouane <pro.florian.michel@gmail.com>
2015-08-25 16:42:20 +0200

    edit BookController

---
+++ app/Controller/BookController.php
@@ -0,0 +1,12 @@
+<?php 
+namespace Controller;
+use \Manager\Manager;
+
+class BookController extends Controller
+{
+	// public function catalogue(){
+	// 	$catManager = new Manager();
+	// 	$user = $userManager->find($userId);
+	// 	$this->show('catalogue');
+	// }
+}
