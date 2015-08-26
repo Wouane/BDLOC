deb01d45bbe1ac72ea25cb9992f6ac1fd8fdd3b5
Web-God <alvardsandz@gmail.com>
2015-08-26 09:59:29 +0200

    Change findAll on BookController

--- app/Controller/BookController.php
+++ app/Controller/BookController.php
@@ -8,7 +8,7 @@ class BookController extends Controller
 	public function catalogue(){
 		$user = $this->getUser();
 		$bookManager = new BookManager();
-		$books = $bookManager->findAll("cover");
+		$books = $bookManager->findAll("title","DESC");
 		$this->show('book/catalogue',["books"=>$books]);
 	}
 }
