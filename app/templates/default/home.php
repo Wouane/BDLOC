f623a50ac39bdef8e04fa0c63f3e6f139ee26d92
Florian Wouane <pro.florian.michel@gmail.com>
2015-08-26 09:57:03 +0200

    edit home.php

--- app/templates/default/home.php
+++ app/templates/default/home.php
@@ -4,13 +4,17 @@
 
 
 <?php $this->start('main_content') ?>
-	<h1>Bienvenue sur la page d'Accueil</h1>
-
-	<div class="presentation">
-		<p>BDLOC est un site de location de Bande dessinés
-		réservé aux Parisiens</p>
-		<br/>
-		<p>Réalisé par D.G | F.M | J.M | A.D</p>
-		<a href="<?= $this->url('inscription') ?>" title="Abonnement">S'abonner</a> | <a href="<?= $this->url('connexion') ?>" title="Connexion">Connexion</a>
+	<div class="main-home">
+		<h1 class="titre-home">Bienvenue sur la page d'Accueil</h1>
+		
+		<div class="presentation">
+			<p>BDLOC est un site de location de Bande dessinés
+			réservé aux Parisiens</p>
+			<br/>
+			<p>Réalisé par D.G | F.M | J.M | A.D</p>
+			<div class="presentation-link">
+				<a href="<?= $this->url('inscription') ?>" title="Abonnement">S'abonner</a> | <a href="<?= $this->url('connexion') ?>" title="Connexion">Connexion</a>
+			</div>
+		</div>
 	</div>
 <?php $this->stop('main_content') ?>
