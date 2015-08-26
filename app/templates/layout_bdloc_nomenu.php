95fe6d5aca543c59c5ac32418166dabbfc5a030b
Florian Wouane <pro.florian.michel@gmail.com>
2015-08-26 15:57:22 +0200

    edit layout_bdloc_nomenu.php

--- app/templates/layout_bdloc_nomenu.php
+++ app/templates/layout_bdloc_nomenu.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?= $this->e($title) ?></title>
-	<link rel="stylesheet" href="<?= $this->assetUrl('css/reset.css') ?>">
+	<!-- <link rel="stylesheet" href="<?= $this->assetUrl('css/reset.css') ?>"> -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="<?= $this->assetUrl('css/style.css') ?>">
 </head>
