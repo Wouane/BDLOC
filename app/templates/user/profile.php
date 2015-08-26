d20f4e8439054446c1bb10264d3aa084d1c03e9a
Alexandre <duparc.alexandre94@gmail.com>
2015-08-26 14:58:18 +0200

    Edit profil fieldset and icon

--- app/templates/user/profile.php
+++ app/templates/user/profile.php
@@ -3,56 +3,69 @@
 <?php $this->start('main_content'); ?>
 
 	<form method="POST" enctype="multipart/form-data">
-		
+	
 	<div class="form-modify">
 		<label for="picture">Modifiez votre image :</label>
 		<input type="file" name="picture" id="picture" />
 	</div>
 
-	<h1>Modifiez les informations générales</h1>
 	
-	<div class="form-modify">
-		<label for="email">Email</label>
-		<input name="email" type="email" class="form_modify" value="<?= $w_user['email']; ?>">
-	</div>
+	<fieldset>
+		<legend>Modifiez les informations générales</legend>
+	<fieldset>
+		<legend>Famille</legend>
+		<div class="form-modify">
+			<label for="firstname"><i class="fa fa-street-view"></i> Prénom</label>
+			<input name="firstname"type="text" id="firstname" class="form_modify" value="<?= $w_user['firstname']; ?>">
+		</div>
 
-	<div class="form-modify">
-		<label for="username">Pseudo</label>
-		<input name="username" type="text" class="form_modify" value="<?= $w_user['username']; ?>">
-	</div>
+		<div class="form-modify">
+			<label for="lastname"><i class="fa fa-street-view"></i> Nom</label>
+			<input name="lastname" type="text" id="lastname" class="form_modify" value="<?= $w_user['lastname']; ?>">
+		</div>
+	</fieldset>
 
-	<div class="form-modify">
-		<label for="firstname">Prénom</label>
-		<input name="firstname"type="text" id="firstname" class="form_modify" value="<?= $w_user['firstname']; ?>">
-	</div>
+	<fieldset>
+		<legend>Identifiant</legend>
+		<div class="form-modify">
+			<label for="email"><i class="fa fa-envelope-o"></i> Email</label>
+			<input name="email" type="email" class="form_modify" value="<?= $w_user['email']; ?>">
+		</div>
 
-	<div class="form-modify">
-		<label for="lastname">Nom</label>
-		<input name="lastname" type="text" id="lastname" class="form_modify" value="<?= $w_user['lastname']; ?>">
-	</div>
-		
-	<div class="form-modify">
-		<label for="streetnumber">Numéro</label>
-		<input name="streetnumber" type="text" id="streetnumber" class="form_modify" value="<?= $w_user['street_number']; ?>">
+		<div class="form-modify">
+			<label for="username"><i class="fa fa-user"></i> Pseudo</label>
+			<input name="username" type="text" class="form_modify" value="<?= $w_user['username']; ?>">
+		</div>
+	</fieldset>
 
-	<div class="form-modify">
-		<label for="streetname">Adresse</label>
-		<input name="streetname" type="text" id="streetname" class="form_modify" value="<?= $w_user['street_name']; ?>">
-	</div>
+	<fieldset>
+		<legend>Adresse postale</legend>
+		<div class="form-modify">
+			<label for="streetnumber"><i class="fa fa-home"></i> Numéro</label>
+			<input name="streetnumber" type="text" id="streetnumber" class="form_modify" value="<?= $w_user['street_number']; ?>">
 
-	<div class="form-modify">
-		<label for="zipcode">Code postal</label>
-		<input name="zipcode" type="text" id="zipcode" class="form_modify" value="<?= $w_user['zip_code']; ?>">
-	</div>
+			<div class="form-modify">
+				<label for="streetname"><i class="fa fa-home"></i> Rue</label>
+				<input name="streetname" type="text" id="streetname" class="form_modify" value="<?= $w_user['street_name']; ?>">
+			</div>
 
-	<div class="form-modify">
-		<label for="phonenumber">N° Mobile ou Fixe</label>
-		<input name="phonenumber" type="text" id="phonenumber" class="form_modify" value="<?= $w_user['phone_number']; ?>">
-	</div>
+			<div class="form-modify">
+				<label for="zipcode"><i class="fa fa-home"></i> Code postal</label>
+				<input name="zipcode" type="text" id="zipcode" class="form_modify" value="<?= $w_user['zip_code']; ?>">
+			</div>
+	</fieldset>
+
+	<fieldset>
+		<legend>Numéro de téléphone</legend>
+		<div class="form-modify">
+			<label for="phonenumber"><i class="fa fa-mobile"></i> N° Mobile ou Fixe</label>
+			<input name="phonenumber" type="text" id="phonenumber" class="form_modify" value="<?= $w_user['phone_number']; ?>">
+		</div>
+	</fieldset>
 
 	<button type="submit" class="btn btn-modify">Valider</button>
 	<div><?= $error; ?></div>
-
+	</fieldset>
 	</form>
 
 <?php $this->stop('main_content'); ?>
