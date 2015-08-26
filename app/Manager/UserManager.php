c04b89454d61bc0f76dff950561018356b7ec91b
Florian Wouane <pro.florian.michel@gmail.com>
2015-08-26 15:59:37 +0200

    edit UserManager

--- app/Manager/UserManager.php
+++ app/Manager/UserManager.php
@@ -7,4 +7,18 @@
 			$this->dbh->query("TRUNCATE users;");
 			
 		}
+
+		public function getToken($token)
+		{
+			$sql = "SELECT * FROM users
+			WHERE token = :token";
+
+			$sth = $this->dbh->prepare($sql);
+			$sth->bindValue(":token", $token);
+			$sth->execute();
+	
+			$foundUser = $sth->fetch();
+
+			return $foundUser;
+		}
 	}
