4679521e0cf334d8d31ae4f3705261ab6d28e02b
Web-God <alvardsandz@gmail.com>
2015-08-27 10:39:38 +0200

    modal windows for login

--- app/templates/default/home.php
+++ app/templates/default/home.php
@@ -5,6 +5,7 @@
 
 <?php $this->start('main_content') ?>
 	<div class="main-home">
+		<div class="login"><?php $this->insert('user/login_insert',['username'=>'','error'=>'']) ?></div>
 		<h1 class="titre-home">Bienvenue sur la page d'Accueil</h1>
 		
 		<div class="presentation">
@@ -13,7 +14,7 @@
 			<br/>
 			<p>Réalisé par D.G | F.M | J.M | A.D</p>
 			<div class="presentation-link">
-				<a href="<?= $this->url('inscription') ?>" title="Abonnement">S'abonner</a> | <a href="<?= $this->url('connexion') ?>" title="Connexion">Connexion</a>
+				<a href="<?= $this->url('inscription') ?>" title="Abonnement">S'abonner</a> | <button>Connexion</button>
 			</div>
 		</div>
 	</div>
