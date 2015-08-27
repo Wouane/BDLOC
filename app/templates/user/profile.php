4aa767520fdd85c823e615e1c842e99ef2bb0230
Florian Wouane <pro.florian.michel@gmail.com>
2015-08-27 16:54:21 +0200

    edit profile.php

--- app/templates/user/profile.php
+++ app/templates/user/profile.php
@@ -8,7 +8,7 @@
 		<legend>Identifiant</legend>
 		<div class="form-modify">
 			<label for="picture">Modifiez votre image :</label>
-			<input type="file" name="pic_name" id="pic_name" value="<?php echo $_SESSION['users']['pic_name']; ?>"/>
+			<input type="file" name="pic_name" id="pic_name"/>
 		</div>
 
 		<div class="form-modify">
