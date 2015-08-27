23601b0d9c0b557502d5b59b60a9c7929c63cbe8
Web-God <alvardsandz@gmail.com>
2015-08-27 11:59:14 +0200

    images for sliders

--- app/templates/book/catalogue.php
+++ app/templates/book/catalogue.php
@@ -121,7 +121,7 @@
 					<option value="publication" id="byDatePublish">Date de publication</option>
 				</select>
 
-				<select name="byNumber" onChange="this.form.submit()" id="byNumber">					
+				<select name="byNumber" onchange="showUser(this.value)" id="byNumber">					
 					<option value="20">Afficher 20 résultats</option>
 					<option value="40">Afficher 40 résultats</option>
 					<option value="60">Afficher 60 résultats</option>
