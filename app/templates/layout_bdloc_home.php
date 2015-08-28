102d58fa7b00a86c3caeb9692b51b92c0b74e65e
Florian Wouane <pro.florian.michel@gmail.com>
2015-08-28 10:37:20 +0200

    add encart facebook to footer layout

--- app/templates/layout_bdloc_home.php
+++ app/templates/layout_bdloc_home.php
@@ -36,6 +36,15 @@
 			</ul>
 		</footer>
 
+		<!-- ENCART FACEBOOK -->
+			<div class="fb-page" data-href="https://www.facebook.com/bdloc" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="false">
+				<div class="fb-xfbml-parse-ignore">
+					<blockquote cite="https://www.facebook.com/bdloc">
+						<a href="https://www.facebook.com/bdloc">Bdloc</a>
+					</blockquote>
+				</div>
+			</div>
+
 	</div>
 </body>
 </html>
