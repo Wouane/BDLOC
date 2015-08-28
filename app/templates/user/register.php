183340179fd4554966e8cac355f4237db0cb37f7
Web-God <alvardsandz@gmail.com>
2015-08-28 17:39:23 +0200

    modified register

--- app/templates/user/register.php
+++ app/templates/user/register.php
@@ -7,19 +7,19 @@
 			<fieldset>
 				<legend>Identifiant</legend>
 				<div class="form-group">
-					<label for="email">Votre Email</label>
+					<label for="email">Votre Email : </label>
 					<input name='email' type="email" class="form-control" id="email" value="<?= $email ?>">
 				</div>
 				<div class="form-group">
-					<label for="username">Choisissez un Pseudo</label>
+					<label for="username">Choisissez un Pseudo : </label>
 					<input name='username' type="text" class="form-control" id="username" value="<?= $username ?>">
 				</div>
 				<div class="form-group">
-					<label for="firstname">Prénom</label>
+					<label for="firstname">Prénom : </label>
 					<input name='firstname' type="text" class="form-control" id="firstname" value="<?= $firstname ?>">
 				</div>
 				<div class="form-group">
-					<label for="lastname">Nom</label>
+					<label for="lastname">Nom : </label>
 					<input name='lastname' type="text" class="form-control" id="lastname" value="<?= $lastname ?>">
 				</div>
 			</fieldset>
@@ -27,19 +27,19 @@
 			<fieldset>
 				<legend>Adresse postale</legend>
 				<div class="form-group">
-					<label for="streetnumber">Numéro</label>
+					<label for="streetnumber">Numéro : </label>
 					<input name='streetnumber' type="text" class="form-control" id="streetnumber" value="<?= $streetnumber ?>">
 				</div>
 				<div class="form-group">
-					<label for="streetname">Adresse</label>
+					<label for="streetname">Adresse : </label>
 					<input name='streetname' type="text" class="form-control" id="streetname" value="<?= $streetname ?>">
 				</div>
 				<div class="form-group">
-					<label for="zipcode">Code Postal</label>
+					<label for="zipcode">Code Postal : </label>
 					<input name='zipcode' type="text" class="form-control" id="zipcode" value="<?= $zipcode ?>">
 				</div>
 				<div class="form-group">
-					<label for="phonenumber">N° Mobile ou Fixe</label>
+					<label for="phonenumber">N° Mobile ou Fixe : </label>
 					<input name='phonenumber' type="text" class="form-control" id="phonenumber" value="<?= $phonenumber ?>">
 				</div>
 			</fieldset>	
@@ -47,7 +47,7 @@
 			<fieldset>
 				<legend>Mot de passe</legend>		
 				<div class="form-group">
-					<label for="password">Votre mot de passe</label>
+					<label for="password">Votre mot de passe : </label>
 					<input name='password' type="password" class="form-control" id="password" value="">
 				</div>
 				<div class="form-group">
