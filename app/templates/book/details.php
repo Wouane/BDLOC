22e4e9ea4a5fd40815dc7c52fc468587d75dfb55
Alexandre <duparc.alexandre94@gmail.com>
2015-08-28 17:06:23 +0200

    details

--- app/templates/book/details.php
+++ app/templates/book/details.php
@@ -2,13 +2,16 @@
 
 <?php $this->start('main_content') ?>
 <div class="details">
-	<div>Titre : <?php echo $book['title']; ?></div>
-	<div>Stock : <?php echo $book['stock']; ?></div>
-	<div>Editeur : <?php echo $book['publisher']; ?></div>
-	<div>Scenarist : <?php echo $book['sfirstname']; ?> <?php echo $book['slastname']; ?></div>
-	<div>Colorist : <?php echo $book['cfirstname']; ?> <?php echo $book['clastname']; ?></div>
-	<div>Illustrator : <?php echo $book['ifirstname']; ?> <?php echo $book['ilastname']; ?></div>
-<div><img src="<?php echo $this->assetUrl('img/couvertures-medium/'. $book["cover"]); ?>"> </div>
+	<div class="img_details"><img src="<?php echo $this->assetUrl('img/couvertures-medium/'. $book["cover"]); ?>"> </div>
+	<div class="text_details">
+		<div>Titre : <?php echo $book['title']; ?></div>
+		<div>Stock : <?php echo $book['stock']; ?></div>
+		<div>Editeur : <?php echo $book['publisher']; ?></div>
+		<div>Scenarist : <?php echo $book['sfirstname']; ?> <?php echo $book['slastname']; ?></div>
+		<div>Colorist : <?php echo $book['cfirstname']; ?> <?php echo $book['clastname']; ?></div>
+		<div>Illustrator : <?php echo $book['ifirstname']; ?> <?php echo $book['ilastname']; ?></div>
+	</div>
+
 </div>
 <?php $this->stop('main_content') ?> 
 
