0e0cadc326c6176e7cf4c7c99542190f47190258
Florian Wouane <pro.florian.michel@gmail.com>
2015-09-01 11:34:34 +0200

    edit profile.php

--- app/templates/user/profile.php
+++ app/templates/user/profile.php
@@ -20,72 +20,83 @@
 		</nav>
 	<ul class="cd-tabs-content">
 		<li data-content="inbox" class="selected">
-			<h3 class="titre-section">Identifiants</h3>
-		<form method="POST" enctype="multipart/form-data">
+		<form class="update-profile" method="POST" enctype="multipart/form-data">
+			<h3 class="titre-section"><i class="fa fa-user"></i> Identifiants</h3>
 				<br/>
 			<!-- NOM -->
 				<div class="form-modify">
-					<label for="lastname"><i class="fa fa-street-view"></i></label>
+					<label for="lastname"> NOM</label>
 					<input name="lastname" placeholder="Prenom" type="text" id="lastname" class="form_modify" value="<?= $w_user['lastname']; ?>">
 				</div>
 			<!-- PRENOM -->
 				<div class="form-modify">
-					<label for="firstname"><i class="fa fa-street-view"></i></label>
+					<label for="firstname"> PRENOM</label>
 					<input name="firstname" placeholder="Nom" type="text" id="firstname" class="form_modify" value="<?= $w_user['firstname']; ?>">
 				</div>
 			<!-- PSEUDO-->
 				<div class="form-modify">
-					<label for="username"><i class="fa fa-user"></i></label>
+					<label for="username"> PSEUDO</label>
 					<input name="username" placeholder="Pseudo" type="text" class="form_modify" value="<?= $w_user['username']; ?>">
 				</div>
 			<!-- EMAIL-->
 				<div class="form-modify">
-					<label for="email"><i class="fa fa-envelope"></i></label>
+					<label for="email"> EMAIL</label>
 					<input name="email" placeholder="Email" type="email" class="form_modify" value="<?= $w_user['email']; ?>">
 				</div>
-				<hr>	
-				<h3 class="titre-section">Mon Adresse</h3>
+				<br/>	
+				<h3 class="titre-section"><i class="fa fa-home"></i> Mon Adresse</h3>
 				<br/>
 			<!--  ADRESS -->		
 				<div class="form-modify">
-					<label for="streetname"><i class="fa fa-home"></i></label>
+					<label for="streetname"> ADRESSE</label>
 					<input name="streetname" placeholder="Adresse" type="text" id="streetname" class="form_modify" value="<?= $w_user['street_name']; ?>">
 				</div>
 			<!--  ZIPCODE -->
 				<div class="form-modify">
-					<label for="zipcode"><i class="fa fa-home"></i></label>
+					<label for="zipcode"> CODE POSTAL</label>
 					<input name="zipcode" placeholder=" Code postal" type="text" id="zipcode" class="form_modify" value="<?= $w_user['zip_code']; ?>">
 				</div>
 			<!-- CITY -->
 				<div class="form-modify">
-					<p>Paris</p>					
+					<label for="city"> VILLE</label>
+					<input name="zipcode" placeholder=" Code postal" type="text" id="zipcode" class="form_modify" value="PARIS">
 				</div>
+		
 			<!--PHONE -->
-				<hr>
-				<h3 class="titre-section">Numéro de téléphone</h3>
+				<br/>
+				<h3 class="titre-section">Numéro</h3>
 				<br/>
 				<div class="form-modify">
-					<label for="phonenumber"><i class="fa fa-mobile"></i></label>
+					<label for="phonenumber"> TELEPHONE </label>
 					<input name="phonenumber" placeholder="Numéro Mobile / Fixe" type="text" id="phonenumber" class="form_modify" value="<?= $w_user['phone_number']; ?>">
 				</div>
 				<!-- VALIDATION -->
-				<button type="submit" class="btn btn-modify">Valider</button>
+				<button type="submit" class="btn btn-default">Valider</button>
 				<div><?= $error; ?></div>
 			
 		</li>
 
 		<li data-content="new">
 				<div class="form-modify">
+					<div class="profil-picture">
+					<p>Photo actuelle</p>
+  					<img class="edit-picture" src="<?= $this->assetUrl('img/uploads/thumbnails/' . $w_user['pic_name'])?>" alt="Photo du profil Utilisateur">
+  					</div>
+					
 					<label for="picture">Modifiez votre image :</label>
 					<input type="file" name="pic_name" id="pic_name"/>
+					<button type="submit" class="btn btn-default">Valider</button>
 				</div>
-				<button type="submit" class="btn btn-modify">Valider</button>
 
 		</li>
 
 		<li data-content="gallery">
-				<a href="<?= $this->url('changeMotdepasse'); ?>" title="password" >Modifier son mot de passe</a>
-				<a href="" title="suppression du compte">Supprimer mon compte Bdloc</a>
+				<a href="<?= $this->url('changeMotdepasse'); ?>" title="password">
+					<i class="fa fa-unlock"></i> Modifier mon mot de passe
+				</a>
+				<a href="" title="suppression du compte" onclick="return(confirm('Etes-vous sûr de vouloir supprimer votre compte ?'));">
+					<i class="fa fa-trash-o"></i>Supprimer mon compte Bdloc
+				</a>
 		</li>
 	</ul> <!-- cd-tabs-content -->
 </div> <!-- cd-tabs -->
