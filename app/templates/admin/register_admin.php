7eb21e7c71b94430e38e16d42287ce3eb94b2328
Florian Wouane <pro.florian.michel@gmail.com>
2015-09-02 14:31:12 +0200

    edit register admin.php

--- app/templates/admin/register_admin.php
+++ app/templates/admin/register_admin.php
@@ -5,23 +5,22 @@
 <form method="POST" name="register_admin" id="register_admin">
 	
 
-	<fieldset>
-		<legend>Enregistrez vous</legend>	
+	<fieldset>	
 		<div class="form_admin">
 			<label for="email">Email : </label>
-			<input  type="email" id="email" name="email">
+			<input  type="email" id="email" name="email" placeholder="Email">
 		</div>
 		<div class="form_admin">
 			<label for="username">Choisissez un Pseudo : </label>
-			<input  type="text"  id="username" name='username'>
+			<input  type="text"  id="username" name='username' placeholder="Pseudo">
 		</div>
 		<div class="form_admin">
 			<label for="password">Votre mot de passe : </label>
-			<input type="password" id="password" name='password' >
+			<input type="password" id="password" name='password' placeholder="Mot de passe">
 		</div>
 		<div class="form_admin">
 			<label for="password_confirm">Confirmer votre mot de passe : </label>
-			<input type="password"  id="password_confirm" name="password_confirm" >
+			<input type="password"  id="password_confirm" name="password_confirm" placeholder="Confirmation">
 		</div>
 		<div class="error_admin">
 			<?php echo $error; ?>
