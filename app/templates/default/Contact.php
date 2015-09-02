d98eeeac5257c1f5ae99ac42f070e805a4ecae94
Alexandre <duparc.alexandre94@gmail.com>
2015-09-02 16:44:11 +0200

    Modify / style contact

--- app/templates/default/Contact.php
+++ app/templates/default/Contact.php
@@ -2,32 +2,32 @@
 
 <?php $this->start('main_content'); ?>
 
-<form method="POST">
-	<div>
-		<label for="name">* Nom complet</label>
+<form method="POST" class="contact">
+	<div class="form-group">
+		<label for="name">NOM COMPLET <sup><i class="fa fa-asterisk"></i></sup></label>
 		<input type="text" id="nom" name="name">
 	</div>
 
-	<div>
-		<label for="email">* Adresse email</label>
+	<div class="form-group">
+		<label for="email">EMAIL <sup><i class="fa fa-asterisk"></i></sup></label>
 		<input type="email" id="email" name="email">
 	</div>
 
-	<div>
-		<label for="subject">* Objet</label>
+	<div class="form-group">
+		<label for="subject">OBJET <sup><i class="fa fa-asterisk"></i></sup></label>
 		<input type="text" id="subject" name="subject">
 	</div> 
 
-	<div>
-		<label for="Message">* Message :</label>
+	<div class="form-group">
+		<label for="Message">MESSAGE <sup><i class="fa fa-asterisk"></i></sup></label>
 		<textarea id="message" name="message"></textarea>
 	</div>
 
-	<div>
+	<div class="obligatoires">
 		<p>* Champs obligatoire</p>
 	</div>	
 
-	<input type="submit" value="Envoyer">
+	<input type="submit" class="validate-contact"value="Envoyer">
 	<div><?php echo $result; ?></div>
 </form>
 
--- app/templates/layout_bdloc.php
+++ app/templates/layout_bdloc.php
@@ -30,6 +30,8 @@
 				<li class="link-catalog"><a href="<?= $this->url('catalogue') ?>" title="Catalogue"><i class="fa fa-book"></i> Les BDs !</a></li>
 				<li class="link-profile"><a href="<?= $this->url('profile') ?>" title="profil"><i class="fa fa-user"></i> Mon compte</a></li>
 				<li class="link-panier"><a href="<?= $this->url('panier') ?>" title="Panier"><i class="fa fa-shopping-cart"></i> Mon panier [ 0 ]</a></li>
+				<li class="link-panier"><a href="<?= $this->url('contact'); ?>" title="Contact"><i class="fa fa-phone"></i> Contactez-nous</a></li>
+				
 			</ul>
 		</nav>
 <!-- ESPACE USER -->
@@ -67,8 +69,7 @@
 				</div>
 			</div>
 			<ul class="nav-footer">				
-				<li><a href="<?= $this->url('mentionsLegales'); ?>">Mentions légales</a></li> |
-				<li><a href="<?= $this->url('contact'); ?>">Contactez-nous</a></li> |
+				<li><a href="<?= $this->url('mentionsLegales'); ?>">Mentions légales</a></li> 
 			</ul>
 		</footer>
 		
