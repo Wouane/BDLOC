501c3c74315c0c298dbc2a224752ff77d92017aa
Florian Wouane <pro.florian.michel@gmail.com>
2015-09-07 12:41:16 +0200

    edit reset password

--- app/templates/user/reset_password.php
+++ app/templates/user/reset_password.php
@@ -2,28 +2,18 @@
 
 <?php $this->start('main_content'); ?>
 
-<form method="POST">
-		<h1>Modifiez votre mot de passe</h1>
+<form method="POST" class="reset-password">
+		<h2 class="reset-password-title">Modifiez votre mot de passe</h2>
 			<div>
 				<label for="password">Nouveau mot de passe</label>
 				<input type="password" name="password" id="password">
 			</div>
-
 			<div>
 				<label for="password_confirm">Confirmation mot de passe</label>
 				<input type="password" name="password_confirm" id="password_confirm">
 			</div>
-			<div>
-				<?php 
-					echo $error;
-				 ?>
-			</div>
-			<div>
-				<?php 
-					echo $succes;
-				 ?>
-			</div>
-
+			<div><?php echo $error; ?></div>
+			<div><?php echo $succes; ?></div>
 			<input type="submit" value="Sauvegarder">
 	</form>
 
