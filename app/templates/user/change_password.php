954100e639e4d1cf2da6983b7cc46940b7da45ab
Florian Wouane <pro.florian.michel@gmail.com>
2015-09-07 12:41:07 +0200

    edit change password

--- app/templates/user/change_password.php
+++ app/templates/user/change_password.php
@@ -2,34 +2,24 @@
 
 <?php $this->start('main_content'); ?>
 
-<form method="POST">
-		<h1>Modifiez votre mot de passe</h1>
-			<div>
+<h2 class="change-password-title">Modifiez votre mot de passe</h2>
+<form method="POST" class="change-password">
+			<div class="change-pswd">
 				<label for="old_password">Ancien mot de passe</label>
 				<input type="password" name="old_password" id="old_password" />
 			</div>
-
-			<div>
+			<div class="change-pswd">
 				<label for="password">Nouveau mot de passe</label>
 				<input type="password" name="password" id="password">
 			</div>
-
-			<div>
+			<div class="change-pswd">
 				<label for="password_confirm">Confirmation mot de passe</label>
 				<input type="password" name="password_confirm" id="password_confirm">
 			</div>
-			<div>
-				<?php 
-					echo $error;
-				 ?>
-			</div>
-			<div>
-				<?php 
-					echo $succes;
-				 ?>
-			</div>
+			<div><?php echo $error; ?></div>
+			<div><?php echo $succes;?></div>
 
-			<input type="submit" value="Sauvegarder">
+			<input class="validate-change-pswd" type="submit" value="Sauvegarder">
 	</form>
 
 <?php $this->stop('main_content'); ?>
