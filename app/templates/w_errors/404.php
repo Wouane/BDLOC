010b217db2a455f47fd04b3c53c77107b5f6839c
Alexandre <duparc.alexandre94@gmail.com>
2015-09-07 14:35:53 +0200

    Put layout_bdloc and css

--- app/templates/w_errors/404.php
+++ app/templates/w_errors/404.php
@@ -1,6 +1,5 @@
-<?php $this->layout('layout', ['title' => 'Error 404 ']) ?>
+<?php $this->layout('layout_bdloc', ['title' => 'Error 404 ']) ?>
 
 <?php $this->start('main_content'); ?>
-<h1>Page non trouvé</h1>
-<img src="<?= $this->assetUrl('img/error404.jpg') ?>">
+<img class="error" src="<?= $this->assetUrl('img/error404.jpg') ?>">
 <?php $this->stop('main_content'); ?>
