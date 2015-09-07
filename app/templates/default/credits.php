0180b2f1b59785d0c342d2c7cdd65ada3a40db69
Florian Wouane <pro.florian.michel@gmail.com>
2015-09-07 12:40:22 +0200

    change text mentions legales - credit + css

--- app/templates/default/credits.php
+++ app/templates/default/credits.php
@@ -2,34 +2,40 @@
 
 <?php $this->start('main_content'); ?>
 
-<div class="mention_body">
-<p>
-	<strong>BDLOC</strong>
+<div class="mentions-body">
+	<h1 class="title-mentions"><i class="fa fa-file-text-o"></i> BDLOC - Mentions légales</h1>
+	
+	<p>Le site www.bdloc.dev est une publication du groupe D.G | F.M | J.M | A.D.
+	<p>bdloc est un projet au capital de 0 euros immatriculée au Registre du Commerce et des Sociétés datant du 11/09/2015.<p>
+	</br>
+	<h2 class="mentions_legales">DECLARATION A LA CNIL</h2>
 
-	<br>Le site www.bdloc.dev est une publication du groupe D.G | F.M | J.M | A.D.<br>
-	bdloc est un projet au capital de 0 euros immatriculée au Registre du Commerce et des Sociétés datant du 11/09/2015 situé au 136 avenue pablo picasso 92000 - NANTERRE.
-	<br>
-	<br><b>DECLARATION A LA CNIL</b>
-	<br>
-	<br>En application de la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers, aux libertés et aux traitements de la vie privée soumis à cette loi, le site www.dbloc.dev est enregistré à la Commission Nationale de l'Informatique et des Libertés (CNIL) selon la déclaration simplifiée n°48 sous la demande d'avis n°1539578.
-	<br>Vous pouvez vister le site Internet de la CNIL à ce sujet : <a href="http://www.cnil.fr/" style="color: blue; text-decoration: underline;" target="_blank">www.cnil.fr</a>
-	<br>
-	<br><b>DROIT D'ACCES ET DE RECTIFICATION</b>
-	<br>
-	<br>Les informations nomitatives et données à caractère personnel vous concernant font l'objet d'un traitement automatisé. Ces données sont conservées dans des serveurs de la société bdloc -
-	<br>bdloc s'engage à ne pas communiquer ces données à des tiers, sous forme de prêt, d'échange, ou de vente de fichier. 
-	<br>Conformément à la loi n°78-17 du 6 janvier, vous bénéficiez d'un droit d'accès à ces informations et, le cas échant, du droit de rectification, ainsi que d'un droit d'opposition à leur conversation.
-	<br>
-	<br>Si vous souhaitez exercer ce droit, il vous suffit de nous contacter :
-	<br>Par courrier électronique à l'adresse suivante : <a href="mailto:bdlocbdloc@gmail.com" style="color: blue; text-decoration: underline;" target="_blank">bdlocbdloc@gmail.com</a>
-	<br>
-	<br><b>CONDITIONS D'UTILISATION</b>
-	<br>
-	<br>Chaque album et l'ensemble des éléments visuels et écrits qui le composent est protégé par le droit d'auteur, en application des articles L 112-1 et suivants du Code de la propriété intellectuelle.
-	<br>Vous ne pouvez en aucun cas utiliser, copier, reproduire, modifier ou dénaturer cette oeuvre ou ses éléments, y compris sur Internet, sans l'autorisation écrite et préalable de l'éditeur concerné.
-	<br>Ce dernier se réserve le droit de poursuivre tout acte de contrefaçon de ses droits de propriété intellectuelle, y compris dans le cadre d'une action pénale.
+	<p>En application de la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers, aux libertés et aux traitements de la vie privée soumis à cette loi, le site www.dbloc.dev est enregistré à la Commission Nationale de l'Informatique et des Libertés (CNIL) selon la déclaration simplifiée n°48 sous la demande d'avis n°1539578.</p>
+	<p>Vous pouvez vister le site Internet de la CNIL à ce sujet : <i class="fa fa-external-link"></i><a href="http://www.cnil.fr/" style="text-decoration: underline;" target="_blank"> www.cnil.fr</a><p>
+	</br>
+	<h2 class="mentions_legales">DROIT D'ACCES ET DE RECTIFICATION</h2>
 
-</p>
+	<p>Les informations nomitatives et données à caractère personnel vous concernant font l'objet d'un traitement automatisé. Ces données sont conservées dans des serveurs de la société bdloc -</p>
+	<p>bdloc s'engage à ne pas communiquer ces données à des tiers, sous forme de prêt, d'échange, ou de vente de fichier. </p>
+	<p>Conformément à la loi n°78-17 du 6 janvier, vous bénéficiez d'un droit d'accès à ces informations et, le cas échant, du droit de rectification, ainsi que d'un droit d'opposition à leur conversation.</p>
+
+	<p>Si vous souhaitez exercer ce droit, il vous suffit de nous contacter :</p>
+	<p>Par courrier électronique à l'adresse suivante : <i class="fa fa-external-link"></i><a href="mailto:bdlocbdloc@gmail.com" style="color: blue; text-decoration: underline;" target="_blank"> bdlocbdloc@gmail.com</a></p>
+	</br>
+	<h2 class="mentions_legales">CONDITIONS D'UTILISATION</h2>
+
+	<p>Chaque album et l'ensemble des éléments visuels et écrits qui le composent est protégé par le droit d'auteur, en application des articles L 112-1 et suivants du Code de la propriété intellectuelle.</p>
+	<p>Vous ne pouvez en aucun cas utiliser, copier, reproduire, modifier ou dénaturer cette oeuvre ou ses éléments, y compris sur Internet, sans l'autorisation écrite et préalable de l'éditeur concerné.</p>
+	<p>Ce dernier se réserve le droit de poursuivre tout acte de contrefaçon de ses droits de propriété intellectuelle, y compris dans le cadre d'une action pénale.</p>
+	</br>
+	<h2 class="mentions_legales">CONTACT | ADRESSE</h2>
+
+	<address>
+	<p>BDLOC INC © est situé au 136 avenue Pablo Picasso</p>
+	<p>92000 - NANTERRE.</p>
+	<p>Responsable légal : Alexandre DUPARC, Florian MICHEL, Julien Morizot, Denis GODECQ</p>
+	<p>Tél:(+33)1 45 25 35 45</p>
+	</address>
 
 </div>
 
