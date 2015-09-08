900fbeeee3455a442dae58ca69c3669484d90fc2
Web-God <alvardsandz@gmail.com>
2015-09-08 10:43:42 +0200

    refactoring input data

--- app/templates/book/catalogue.php
+++ app/templates/book/catalogue.php
@@ -13,26 +13,23 @@
 				<label for="check_all" class="cheked_label">Check all</label>
 				<br>
 
-				<input type="checkbox" name="genre[]" id="action" value="action" class="chk_boxes">
-				<label for="action">Action</label><br />
+				<input type="checkbox" name="genre[]" id="adaptation" value="adaptation" class="chk_boxes">
+				<label for="adaptation">Adaptation</label><br />
 
-				<input type="checkbox" name="genre[]" id="adulte" value="adulte" class="chk_boxes">
-				<label for="adulte">Adulte</label><br />
+				<input type="checkbox" name="genre[]" id="animalier" value="animalier" class="chk_boxes">
+				<label for="animalier">Animalier</label><br />
 
 				<input type="checkbox" name="genre[]" id="aventure" value="aventure" class="chk_boxes">
 				<label for="aventure">Aventure</label><br />
 
-				<input type="checkbox" name="genre[]" id="aviation" value="aviation" class="chk_boxes">
-				<label for="aviation">Aviation</label><br />
+				<input type="checkbox" name="genre[]" id="biographie" value="biographie" class="chk_boxes">
+				<label for="biographie">Biographie</label><br />
 
-				<input type="checkbox" name="genre[]" id="espionnage" value="espionnage" class="chk_boxes">
-				<label for="espionnage">Espionnage</label><br />
+				<input type="checkbox" name="genre[]" id="comics" value="comics" class="chk_boxes">
+				<label for="comics">Comics</label><br />
 
-				<input type="checkbox" name="genre[]" id="fantastique" value="fantastique" class="chk_boxes">
-				<label for="fantastique">Fantastique</label><br />
-
-				<input type="checkbox" name="genre[]" id="guerre" value="guerre" class="chk_boxes">
-				<label for="guerre">Guerre</label><br />
+				<input type="checkbox" name="genre[]" id="divers" value="divers" class="chk_boxes">
+				<label for="divers">Divers</label><br />
 
 				<input type="checkbox" name="genre[]" id= "heroic_fantasy" value="heroic_fantasy" class="chk_boxes">
 				<label for="heroic_fantasy">Heroic Fantasy</label><br />
@@ -43,38 +40,38 @@
 				<input type="checkbox" name="genre[]" id="humour" value="humour" class="chk_boxes">
 				<label for="humour">Humour</label><br />
 
+				<input type="checkbox" name="genre[]" id="independant" value="independant" class="chk_boxes">
+				<label for="independant">Indépendant</label><br />
+
 				<input type="checkbox" name="genre[]" id="jeunesse" value="jeunesse" class="chk_boxes">
 				<label for="jeunesse">Jeunesse</label><br />
 
-				<input type="checkbox" name="genre[]" id="inclassables" value="inclassables" class="chk_boxes">
-				<label for="inclassables">Inclassables</label><br />
+				
 
-				<input type="checkbox" name="genre[]" id="litterature" value="litterature" class="chk_boxes">
-				<label for="litterature">Littérature</label><br />
+				<input type="checkbox" name="genre[]" id="manga" value="manga" class="chk_boxes">
+				<label for="manga">Manga</label><br />
 
 				<input type="checkbox" name="genre[]" id="polar" value="polar" class="chk_boxes">
 				<label for="polar">Polar</label><br />
 
-				<input type="checkbox" name="genre[]" id="science-fiction" value="science-fiction" class="chk_boxes">
-				<label for="science-fiction">Science Fiction</label><br />
 
-				<input type="checkbox" name="genre[]" id="sentimental" value="sentimental" class="chk_boxes">
-				<label for="sentimental">Sentimental</label><br />
+				<input type="checkbox" name="genre[]" id="quotidien" value="quotidien" class="chk_boxes">
+				<label for="quotidien">Quotidien</label><br />
 
-				<input type="checkbox" name="genre[]" id="sport" value="sport" class="chk_boxes">
-				<label for="sport">Sport</label><br />
+				<input type="checkbox" name="genre[]" id="roman" value="roman" class="chk_boxes">
+				<label for="roman">Roman graphique</label><br />
 
-				<input type="checkbox" name="genre[]" id="super_heros" value="super_heros" class="chk_boxes">
-				<label for="super_heros">Super Héros</label><br />
+				<input type="checkbox" name="genre[]" id="fiction" value="fiction" class="chk_boxes">
+				<label for="fiction">Science Fiction</label><br />
 
-				<input type="checkbox" name="genre[]" id="tranche_de_vie" value="tranche_de_vie" class="chk_boxes">
-				<label for="tranche_de_vie">Tranche de vie</label><br />
+				<!-- <input type="checkbox" name="genre[]" id="tranche_de_vie" value="tranche_de_vie" class="chk_boxes">
+				<label for="tranche_de_vie">Tranche de vie</label><br /> -->
 
 				<input type="checkbox" name="genre[]" id="thriller" value="thriller" class="chk_boxes">
 				<label for="thriller">Thriller</label><br />
 
-				<input type="checkbox" name="genre[]" id="western" value="western" class="chk_boxes">
-				<label for="western">Western</label><br />
+				<!-- <input type="checkbox" name="genre[]" id="western" value="western" class="chk_boxes">
+				<label for="western">Western</label><br /> -->
 				<button type="submit">Chercher</button>
 		</form>
 		<form >
@@ -93,8 +90,8 @@
 		<input type="search" placeholder="Recherche" name="search" id="search">
 		<form method="GET">
 				<select name="byOrder" onChange="this.form.submit()" id="byOrder">
-					<option value="date" id="byDateCreated">Date d'ajout au catalogue</option>
 					<option value="alpha" id="byAlpha">Ordre alphabétique</option>
+					<option value="date" id="byDateCreated">Date d'ajout au catalogue</option>					
 					<option value="publication" id="byDatePublish">Date de publication</option>
 				</select>
 		</form>
@@ -136,6 +133,7 @@
 				echo "<ul>";				
 				echo "<li class='title-bd'>" . $book['ttitle'] . "</li>";
 				echo "<li class='title-bd'>" . $book['title'] . "</li>";
+				echo "<li class='title-bd'>" . $book['catstyle'] . "</li>";				
 				if(isset($book['sfirstname'])){echo "<li>" . $book['sfirstname'] . " " . $book['slastname'] . " <a href='' class='tooltip'><i class='fa fa-info-circle'></i><span>Auteur</span></a></li>";}					
 				if(isset($book['cfirstname'])){echo "<li>" . $book['cfirstname'] . " " . $book['clastname'] . " <a href='' class='tooltip'><i class='fa fa-info-circle'></i><span>Coloriste</span></a></li>";}					
 				if(isset($book['ifirstname'])){echo "<li>" . $book['ifirstname'] . " " . $book['ilastname'] . " <a href='' class='tooltip'><i class='fa fa-info-circle'></i><span>Illustrateur</span></a></li>";}
