cc6ed12d1570302052f90d6561868f6f6361ef57
Web-God <alvardsandz@gmail.com>
2015-09-08 14:14:53 +0200

    fix bug to AJAX search

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,24 +1,32 @@
 
-$('#search').on("keyup", function(){
-	//console.log('julien');
+$('#search').on("keyup", function()
+{
+	
 	var search = $('#search').val();
 	if (search.length < 0) {
 		$('.listcat').html("");
 	}
+
+
 	else {
 			$.ajax({
 				//récupère l'url du formulaire
-				"url":$("#filter").attr("action"),
+				"url":$("#searchform").attr("action"),
+
 				"data":{
-					"search": $("#search").val();
-				}
-			}).done(function(response){
-				var content=$(response).find(".listcat")
+					"search": $("#search").val()
+						}
+
+			}).done(function(response) {
+				var content = $(response).find(".listcat");
+				
 				$('.listcat').html(content);
+				//console.log(content);
 			});	
 		}
 	
 });
-//LOGIN
+
+//<LOGIN></LOGIN>
 
 
