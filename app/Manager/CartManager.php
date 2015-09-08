e52845091eb78afdb38c9be402c1053d0ee6f79f
Florian Wouane <pro.florian.michel@gmail.com>
2015-09-08 15:59:50 +0200

    Begin CartManager.php

--- app/Manager/CartManager.php
+++ app/Manager/CartManager.php
@@ -4,5 +4,22 @@
 	class CartManager extends \W\Manager\Manager
 	{
 
+		public function AddBooktoCart($book_id){
+
+		}
+
+
+		public function ShowBooksintoCart(){
+
+
+			
+
+			$sql = "SELECT *
+					FROM books
+					WHERE id = :id"
+
+
+		}
+
 		
 	}
