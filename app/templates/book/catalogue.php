24864a891b8ec0e3aae6701326c23d1a80bd798f
Web-God <alvardsandz@gmail.com>
2015-09-08 14:29:01 +0200

    delete <br>

--- app/templates/book/catalogue.php
+++ app/templates/book/catalogue.php
@@ -14,70 +14,69 @@
 				<br>
 
 				<input type="checkbox" name="genre[]" id="adaptation" value="adaptation" class="chk_boxes">
-				<label for="adaptation">Adaptation</label><br />
+				<label for="adaptation">Adaptation</label>
 
 				<input type="checkbox" name="genre[]" id="animalier" value="animalier" class="chk_boxes">
-				<label for="animalier">Animalier</label><br />
+				<label for="animalier">Animalier</label>
 
 				<input type="checkbox" name="genre[]" id="aventure" value="aventure" class="chk_boxes">
-				<label for="aventure">Aventure</label><br />
+				<label for="aventure">Aventure</label>
 
 				<input type="checkbox" name="genre[]" id="biographie" value="biographie" class="chk_boxes">
-				<label for="biographie">Biographie</label><br />
+				<label for="biographie">Biographie</label>
 
 				<input type="checkbox" name="genre[]" id="comics" value="comics" class="chk_boxes">
-				<label for="comics">Comics</label><br />
+				<label for="comics">Comics</label>
 
 				<input type="checkbox" name="genre[]" id="divers" value="divers" class="chk_boxes">
-				<label for="divers">Divers</label><br />
+				<label for="divers">Divers</label>
 
 				<input type="checkbox" name="genre[]" id= "heroic_fantasy" value="heroic_fantasy" class="chk_boxes">
-				<label for="heroic_fantasy">Heroic Fantasy</label><br />
+				<label for="heroic_fantasy">Heroic Fantasy</label>
 
 				<input type="checkbox" name="genre[]" id="historique" value="historique" class="chk_boxes">
-				<label for="historique">Historique</label><br />
+				<label for="historique">Historique</label>
 
 				<input type="checkbox" name="genre[]" id="humour" value="humour" class="chk_boxes">
-				<label for="humour">Humour</label><br />
+				<label for="humour">Humour</label>
 
 				<input type="checkbox" name="genre[]" id="independant" value="independant" class="chk_boxes">
-				<label for="independant">Indépendant</label><br />
+				<label for="independant">Indépendant</label>
 
 				<input type="checkbox" name="genre[]" id="jeunesse" value="jeunesse" class="chk_boxes">
-				<label for="jeunesse">Jeunesse</label><br />
-
+				<label for="jeunesse">Jeunesse</label>
 				
 
 				<input type="checkbox" name="genre[]" id="manga" value="manga" class="chk_boxes">
-				<label for="manga">Manga</label><br />
+				<label for="manga">Manga</label>
 
 				<input type="checkbox" name="genre[]" id="polar" value="polar" class="chk_boxes">
-				<label for="polar">Polar</label><br />
+				<label for="polar">Polar</label>
 
 
 				<input type="checkbox" name="genre[]" id="quotidien" value="quotidien" class="chk_boxes">
-				<label for="quotidien">Quotidien</label><br />
+				<label for="quotidien">Quotidien</label>
 
 				<input type="checkbox" name="genre[]" id="roman" value="roman" class="chk_boxes">
-				<label for="roman">Roman graphique</label><br />
+				<label for="roman">Roman graphique</label>
 
 				<input type="checkbox" name="genre[]" id="fiction" value="fiction" class="chk_boxes">
-				<label for="fiction">Science Fiction</label><br />
+				<label for="fiction">Science Fiction</label>
 
 				<!-- <input type="checkbox" name="genre[]" id="tranche_de_vie" value="tranche_de_vie" class="chk_boxes">
-				<label for="tranche_de_vie">Tranche de vie</label><br /> -->
+				<label for="tranche_de_vie">Tranche de vie</label> -->
 
 				<input type="checkbox" name="genre[]" id="thriller" value="thriller" class="chk_boxes">
-				<label for="thriller">Thriller</label><br />
+				<label for="thriller">Thriller</label>
 
 		</form>
 		<form >
 			<h3>Disponibilité</h3>
 
 			<input type="checkbox" name="disponibles" id="disponibles" value="disponibles">
-			<label for="disponibles">Disponibles</label><br />
+			<label for="disponibles">Disponibles</label>
 			<input type="checkbox" name="indisponibles" id="indisponibles" value="indisponibles">
-			<label for="indisponibles">Indisponibles</label><br />
+			<label for="indisponibles">Indisponibles</label>
 		</form>
 	</div>
 <div class="maincat">
@@ -89,7 +88,7 @@
 		</form>
 
 		<form method="GET">
-				<select name="byOrder" onChange="this.form.submit()" id="byOrder">
+				<select name="byOrder" onChange="recupere()" id="byOrder">
 					<option value="alpha" id="byAlpha">Ordre alphabétique</option>
 					<option value="date" id="byDateCreated">Date d'ajout au catalogue</option>					
 					<option value="publication" id="byDatePublish">Date de publication</option>
