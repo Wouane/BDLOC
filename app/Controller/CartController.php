95a5f695b185d6765ec5a0319512f741adbc185e
Florian Wouane <pro.florian.michel@gmail.com>
2015-09-08 12:36:19 +0200

    Not Finish Cart edit CartController

--- app/Controller/CartController.php
+++ app/Controller/CartController.php
@@ -3,9 +3,33 @@ namespace Controller;
 use \Manager\CartManager;
 use \W\Controller\Controller;
 
-class CartController extends Controller 
-{
-	public function cart {
-		$cm = new CartController;
+class CartController extends Controller {
+	publicfunction cart {
+	$cm = new CartController;
 	}
-}
+
+//MONTRER LES LIVRES DANS LE PANIER
+
+// 	public function showBooks()
+// 	{   
+//		$books = $_GET['book_id'];
+// 		$this->allowTo('user');
+// 		$CartManager = new \Manager\CartManager();
+// 		$Cart = $CartManager->findAll("modifiedDate", "DESC");
+// 		// debug($books);
+// 		// print_r(get_class_methods($bookManager));
+// 		$this->show('/catalogue/panier/', ['books' => $books]);
+// 	}
+
+// //DELETE UN BOOK
+
+// 	public function deleteBook($id)
+// 	{   
+// 		$this->allowTo('user');
+// 		$CartManager = new\Manager\CartManager();
+
+// 		// $deleteBooks = $CartManager->delete($id);
+// 		$CartManager->delete($id);
+// 		$this->redirectToRoute('panier');
+// 	}
+// }
