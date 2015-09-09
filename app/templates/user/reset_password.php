f2e0c95c1381d4cd212adeae07e39cdb2deb0958
Florian Wouane <pro.florian.michel@gmail.com>
2015-09-09 14:01:44 +0200

    edit h2 reset password + css

--- app/templates/user/reset_password.php
+++ app/templates/user/reset_password.php
@@ -1,20 +1,20 @@
-<?php $this->layout('layout_bdloc_nomenu', ['title' => 'Mot de passe']) ?>
+<?php $this->layout('layout_bdloc_home', ['title' => 'Mot de passe']) ?>
 
 <?php $this->start('main_content'); ?>
 
+<h2 class="reset-password-title">Modifiez votre mot de passe</h2>
 <form method="POST" class="reset-password">
-		<h2 class="reset-password-title">Modifiez votre mot de passe</h2>
-			<div>
-				<label for="password">Nouveau mot de passe</label>
-				<input type="password" name="password" id="password">
-			</div>
-			<div>
-				<label for="password_confirm">Confirmation mot de passe</label>
-				<input type="password" name="password_confirm" id="password_confirm">
-			</div>
-			<div><?php echo $error; ?></div>
-			<div><?php echo $succes; ?></div>
-			<input type="submit" value="Sauvegarder">
-	</form>
+	<div class="form-reset">
+		<label class="reset-password-label" for="password">NOUVEAU MOT DE PASSE</label>
+		<input type="password" name="password" id="password" placeholder="Nouveau">
+	</div>
+	<div class="form-reset">
+		<label class="reset-password-label" for="password_confirm">CONFIRMATION MOT DE PASSE</label>
+		<input type="password" name="password_confirm" id="password_confirm" placeholder="Confirmation">
+	</div>
+	<div><?php echo $error; ?></div>
+	<div><?php echo $succes; ?></div>
+	<input class="btn-default" type="submit" value="Sauvegarder">
+</form>
 
 <?php $this->stop('main_content'); ?>
