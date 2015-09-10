9e5951d68dc2bfb5517df0271736363a32915359
Jma777 <jmamorizot@aol.fr>
2015-09-10 17:32:24 +0200

    Refactor details.php

--- app/templates/book/details.php
+++ app/templates/book/details.php
@@ -2,13 +2,39 @@
 <div class="details">
 	<div class="img_details"><img src="<?php echo $this->assetUrl('img/couvertures-originales/'. $book["cover"]); ?>"> </div>
 	<div class="text_details">
-		<div><strong>Titre :</strong> <?php echo $book['title']; ?></div>
-		<!-- <div>Résumé : <?php //echo $book['nnote']; ?></div> -->
-		<div><strong>Stock :</strong> <?php echo $book['stock']; ?></div>
-		<div><strong>Editeur :</strong> <?php echo $book['publisher']; ?></div>
-		<div><strong>Scenarist :</strong> <?php echo $book['sfirstname']; ?> <?php echo $book['slastname']; ?></div>
-		<div><strong>Colorist :</strong> <?php echo $book['cfirstname']; ?> <?php echo $book['clastname']; ?></div>
-		<div><strong>Illustrator :</strong> <?php echo $book['ifirstname']; ?> <?php echo $book['ilastname']; ?></div>
+		<div>
+			<h2><?php echo $book['ttitle']; ?></h2>
+		</div>
+		<div>
+			<h3><?php echo $book['title']; ?></h3>
+		</div>
+		<div>
+			<strong>Résumé :</strong>
+			<p><?php echo $book['nnote']; ?></p>
+		</div>
+		<div>
+			<strong>Stock :</strong> 
+			<?php echo $book['stock']; ?>
+		</div>
+		<div>
+			<strong>Editeur :</strong> 
+			<?php echo $book['publisher']; ?>
+		</div>
+		<div>
+			<strong>Scenarist :</strong> 
+			<?php echo $book['sfirstname']; ?> 
+			<?php echo $book['slastname']; ?>
+		</div>
+		<div>
+			<strong>Colorist :</strong> 
+			<?php echo $book['cfirstname']; ?> 
+			<?php echo $book['clastname']; ?>
+		</div>
+		<div>
+			<strong>Illustrator :</strong> 
+			<?php echo $book['ifirstname']; ?> 
+			<?php echo $book['ilastname']; ?>
+		</div>
 	</div>
 </div>
 
