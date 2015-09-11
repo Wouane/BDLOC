ed9b521bd2ef53527bcba48280613ce263204700
Florian Wouane <pro.florian.michel@gmail.com>
2015-09-11 12:04:15 +0200

    add table cart

--- app/templates/cart/Cart.php
+++ app/templates/cart/Cart.php
@@ -5,17 +5,37 @@
 
 	<?php $this->start("main_content") ?>
 	
-	<h2>Bienvenue sur votre panier <span><?= $w_user['username']; ?></span> ! </h2>
-	<div class="main-cart">	
+	<h2 class="cart-title">Vous êtes sur les panier de <span><?= $w_user['username']; ?></span> ! </h2>
+<div class="main-cart">	
+		<table class="my-cart">
+			<caption><i class="fa fa-shopping-cart cart"></i> Mon panier</caption>
+			<thead>
+				<th>Cover</th>
+				<th>Titre</th>
+			</thead>
+			<tbody>
+				
+				<!-- Debut FOREACH pour affichage des livres dans le panier -->
+			<?php foreach($books as $book):?>
+			<tr>
+				<td>
+					<img src="<?php echo $this->assetUrl('img/couvertures-medium/'.$book['cover']);?>" width='150px'>
+				</td>
+				<td>
+					<?= $this->e($book['title'])?>
+				</td>
+				
+			</tr>
+			<?php endforeach ?>
 
-		<?php debug($user)?>
-	
+			</tbody>
+		</table>
 		<!-- ACTIONS DU PANIER -->
 
 	</div>
 		
 	
-		<a href="<?php //echo $this->url('catalogue') ?>" title="Continuer">Continuer à bouquiner</a>
+		<a href="<?php echo $this->url('catalogue') ?>" title="Continuer">Continuer à bouquiner</a>
 		
 
 	<?php $this->stop("main_content") ?>
