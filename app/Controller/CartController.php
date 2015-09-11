82c92235397544408d0a28bc22b99bd3fc40331d
Florian Wouane <pro.florian.michel@gmail.com>
2015-09-11 12:05:15 +0200

    Cart Controller add Cart show books

--- app/Controller/CartController.php
+++ app/Controller/CartController.php
@@ -5,38 +5,46 @@ use \W\Controller\Controller;
 
 class CartController extends Controller{
 
-// 		public function cart(){
-// 			$cm = new CartManager();
-// 			$this->show('cart/Cart');
-// 		}
-// 	//methode pour récupérer l'id du book cliqué
-		public function Cart(){
-		$books="";
-		$cm = new \Manager\CartManager();
-		$user = $this->getUser();
-		$this->show('cart/Cart', ['books' => $books, 'user' => $user]);
-		
-	}
- 
 
+ 	//methode pour récupérer l'id du book cliqué
+	public function showCart()
+		{
+			$cm = new \Manager\CartManager();
+			$cbm = new \Manager\Book_cartManager();
+			$bm = new \Manager\BookManager();
+			// récupérer l'id du cart
+			$user = $this->getUser();
+			$user_id = $user['id'];
+			$cart_id = $cm->findCart($user_id);
+			// récupérer les id des livres 
+			$books_ids = $cbm->findAllBooksInCart($cart_id);
+
+			//récupérer les infos du livre
+			$books = $bm->showBooks($books_ids);
+			//debug($books);
+			
+			$this->show('cart/Cart', ['books' => $books, 'user' => $user]);
+			
+		}
+ 
+ 
 	public function AddbooktoCart($book_id)
-	{   $books="";
-		$cm = new \Manager\CartManager();
-		$cbm = new \Manager\Book_cartManager();
-		$user = $this->getUser();
-		$user_id = $user['id'];
-		$cart_id = $cm-> AddbooktoCart($user_id);
-		$cbm->AddbooktoCart($cart_id, $book_id);
-		$cm->findCard();
-		//$this->allowTo('user');
-		// debug($user);
-		
-		// $Cart = $CartManager->findAll("modifiedDate", "DESC");
-		// $showCart = $cm->ShowCart();
-		// print_r(get_class_methods($bookManager));
-		$this->show('cart/Cart', ['books' => $books, 'user' => $user]);
-		
-	}
+		{   
+			$books="";
+			$cm = new \Manager\CartManager();
+			$cbm = new \Manager\Book_cartManager();
+			$user = $this->getUser();
+			$user_id = $user['id'];
+
+			$cart_id = $cm->findCart($user_id);
+
+			if (empty($cart_id)) {
+				$cart_id = $cm->AddbooktoCart($user_id);
+			}
+			
+			$cbm->AddbooktoCart($cart_id, $book_id);
+			$this->redirectToRoute('catalogue');
+		}
 }
 // //DELETE UN BOOK
 
