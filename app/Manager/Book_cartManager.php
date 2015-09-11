b33af6271b608046f8dcc01e7b541cd511f04235
Florian Wouane <pro.florian.michel@gmail.com>
2015-09-11 12:07:54 +0200

    edit book cart Manager

--- app/Manager/Book_cartManager.php
+++ app/Manager/Book_cartManager.php
@@ -5,13 +5,21 @@
 	{
 				
 		public function AddbooktoCart($cart_id, $book_id){
-			$sql="INSERT INTO book_cart(cart_id, book_id) VALUES ($cart_id, $book_id)";
+			$sql="INSERT INTO book_carts(cart_id, book_id) VALUES ($cart_id, $book_id)";
 			$sth = $this->dbh->prepare($sql);
 			return $sth->execute();
 			
 		}
 
+		public function findAllBooksInCart($cart_id){
+			$sql = "SELECT book_id			
+					FROM book_carts
+					WHERE cart_id = $cart_id";
+			$sth = $this->dbh->prepare($sql);
 
+			$sth->execute();			
+			return $sth->fetchAll();
+		}
 
 	
 	}
