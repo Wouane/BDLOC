d4e0936bb8d95853f6b9dfa9df6d23e7f6822131
Web-God <alvardsandz@gmail.com>
2015-09-11 11:01:31 +0200

    more cart

--- app/Controller/CartController.php
+++ app/Controller/CartController.php
@@ -5,36 +5,36 @@ use \W\Controller\Controller;
 
 class CartController extends Controller{
 
-// 		public function cart(){
-// 			$cm = new CartManager();
-// 			$this->show('cart/Cart');
-// 		}
-// 	//methode pour récupérer l'id du book cliqué
-		public function Cart(){
-		$books="";
-		$cm = new \Manager\CartManager();
-		$user = $this->getUser();
-		$this->show('cart/Cart', ['books' => $books, 'user' => $user]);
-		
-	}
+
+ 	//methode pour récupérer l'id du book cliqué
+	public function Cart()
+		{
+			$books="";
+			$cm = new \Manager\CartManager();
+			$user = $this->getUser();
+			$user_id = $user['id'];
+			$this->show('cart/Cart', ['books' => $books, 'user' => $user]);
+			
+		}
  
 
 	public function AddbooktoCart($book_id)
-	{   $books="";
-		$cm = new \Manager\CartManager();
-		$cbm = new \Manager\Book_cartManager();
-		$user = $this->getUser();
-		$user_id = $user['id'];
-		$cart_id = $cm->AddbooktoCart($user_id);
-		$cbm->AddbooktoCart($cart_id, $book_id);
-		$cm->findCard();
+		{   
+			$books="";
+			$cm = new \Manager\CartManager();
+			$cbm = new \Manager\Book_cartManager();
+			$user = $this->getUser();
+			$user_id = $user['id'];
 
+			$cart_id = $cm->findCart($user_id);
 
-
-		$this->show('cart/Cart', ['books' => $books, 'user' => $user]);
-
-		
-	}
+			if (empty($cart_id)) {
+				$cart_id = $cm->AddbooktoCart($user_id);
+			}
+			
+			$cbm->AddbooktoCart($cart_id, $book_id);
+			$this->show('cart/Cart', ['books' => $books, 'user' => $user]);
+		}
 }
 // //DELETE UN BOOK
 
