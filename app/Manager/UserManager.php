262e263a89d7e3bf7a23fe84d62c16c545997de4
Florian Wouane <pro.florian.michel@gmail.com>
2015-09-11 12:10:04 +0200

    edit UserManager

--- app/Manager/UserManager.php
+++ app/Manager/UserManager.php
@@ -1,77 +1,77 @@
 <?php 
-	namespace Manager;
+namespace Manager;
 
-	class UserManager extends \W\Manager\UserManager
+class UserManager extends \W\Manager\UserManager
+{
+	public function Fakedata(){
+		$this->dbh->query("TRUNCATE users;");
+		
+	}
+
+	public function getToken($token)
 	{
-		public function Fakedata(){
-			$this->dbh->query("TRUNCATE users;");
-			
-		}
+		$sql = "SELECT *
+		FROM users
+		WHERE token = :token";
 
-		public function getToken($token)
-		{
-			$sql = "SELECT *
-					FROM users
-					WHERE token = :token";
+		$sth = $this->dbh->prepare($sql);
+		$sth->bindValue(":token", $token);
+		$sth->execute();
+		
+		$foundUser = $sth->fetch();
 
-			$sth = $this->dbh->prepare($sql);
-			$sth->bindValue(":token", $token);
-			$sth->execute();
-	
-			$foundUser = $sth->fetch();
-
-			return $foundUser;
-		}
+		return $foundUser;
+	}
 
 		// GESTION DE DELETE PROFILE 
-		public function deleteProfile($id)
-		{
-					$sql = "DELETE FROM users
-							WHERE id = :id";
+	public function deleteProfile($id)
+	{
+		$sql = "DELETE FROM users
+		WHERE id = :id";
 
-			$sth = $this->dbh->prepare($sql);
-			$sth->bindValue(":id", $id);
-			$sth->execute();
-		}
-
-		public function countall()
-		{
-			//Qui compte 
-			$sql = "SELECT COUNT(*)
-					FROM users ";
-
-			$sth = $this->dbh->prepare($sql);
-			$sth->execute();
-			$count = $sth->fetchColumn();
-
-			//Return la réponse
-			return $count;
-		}
-
-		public function countallAdmin()
-		{
-			//Qui compte 
-			$sql = "SELECT COUNT(*)
-					FROM admins ";
-
-			$sth = $this->dbh->prepare($sql);
-			$sth->execute();
-			$count_admin = $sth->fetchColumn();
-
-			//Return la réponse
-			return $count_admin;
-		}
-
-		public function countstock()
-		{
-			$sql = "SELECT SUM(stock)
-					FROM books";
-
-			$sth = $this->dbh->prepare($sql);
-			$sth->execute();
-			$countstock = $sth->fetchColumn();
-
-			return $countstock;
-		}
-
+		$sth = $this->dbh->prepare($sql);
+		$sth->bindValue(":id", $id);
+		$sth->execute();
 	}
+
+	public function countall()
+	{
+			//Qui compte 
+		$sql = "SELECT COUNT(*)
+		FROM users ";
+
+		$sth = $this->dbh->prepare($sql);
+		$sth->execute();
+		$count = $sth->fetchColumn();
+
+			//Return la réponse
+		return $count;
+	}
+
+	public function countallAdmin()
+	{
+			//Qui compte 
+		$sql = "SELECT COUNT(*)
+		FROM admins ";
+
+		$sth = $this->dbh->prepare($sql);
+		$sth->execute();
+		$count_admin = $sth->fetchColumn();
+
+			//Return la réponse
+		return $count_admin;
+	}
+
+	public function countstock()
+	{
+		$sql = "SELECT SUM(stock)
+		FROM books";
+
+		$sth = $this->dbh->prepare($sql);
+		$sth->execute();
+		$countstock = $sth->fetchColumn();
+
+		return $countstock;
+	}
+
+}
